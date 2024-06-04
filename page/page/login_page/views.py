6beaacafba423ea31312121f14c88dc7922358af
Kimani <155839003+that-kimani@users.noreply.github.com>
2024-06-04 23:22:39 +0300

    Configurations & Adding Code.
    -URL routing
    -Settings configuration
    -Design brought in.
    -Static files configured

---
+++ page/page/login_page/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def login_page(request):
+    template = loader.get_template('login_page.html')
+
+    return HttpResponse(template.render(request=request))
